415835b0c7a2ca204313563cee95a2bddc306166
Markus Tacker <m@coderbyheart.de>
2014-11-09 13:02:01 +0100

    Initial version.

---
+++ Gruntfile.js
@@ -0,0 +1,49 @@
+module.exports = function (grunt) {
+
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        less: {
+            default: {
+                options: {},
+                files: {
+                    "build/css/styles.css": "src/lesscss/styles.less"
+                }
+            }
+        },
+        copy: {
+            default: {
+                files: [
+                    {expand: true, cwd: 'src/img/', src: ['*'], dest: 'build/img/'},
+                    {expand: true, cwd: 'src/img/favicon', src: ['*'], dest: 'build/img/favicon'}
+                ]
+            }
+        },
+        cssmin: {
+            default: {
+                expand: true,
+                cwd: 'build/css/',
+                dest: 'build/css/',
+                src: ['*.css'],
+                ext: '.css'
+            }
+        },
+        uglify: {
+            default: {
+                files: [
+                    {'build/js/bootstrap.min.js': 'vendor/bootstrap/dist/js/bootstrap.js'},
+                    {'build/js/jquery.min.js': 'vendor/jquery-1.10.2.js'},
+                    {'build/js/angular.min.js': 'node_modules/angular/lib/angular.js'}
+                ]
+            }
+        },
+        clean: ['build/']
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-less');
+    grunt.loadNpmTasks('grunt-contrib-clean');
+    grunt.loadNpmTasks('grunt-contrib-copy');
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+
+    grunt.registerTask('default', ['clean', 'copy', 'less', 'cssmin', 'uglify']);
+};
